631b7963cdcbd83642f52cfcad797f30ea0db0b3
Alanahh <alonavasylenko1996@gmail.com>
2018-11-19 08:52:19 +0200

    basic tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+class Item{
+  note: string;
+  done: boolean;
+  
+   
+  constructor(note: string) {
+
+      this.note = note;
+      this.done = false;
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +20,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todos';
+  text = "";
+  items: Item[] = 
+  [
+      { note: "Хлеб", done: false},
+      { note: "Масло", done: false}
+  ];
+  addItem(text: string): void {
+       console.log(this.text)
+      if(text==null || text.trim()=="")
+          return;
+      this.items.push(new Item(text));
+  }
+  deleteToDo(index){
+
+  this.items.splice(index, 1);
+
 }
+changeToDo(index){
+  var val = this.items.splice(index, 1);
+  console.log(val)
+
+     this.text = val[0].note;
+    console.log(this.text);
+    // document.getElementById("myForm").value = newNote;
+    
+  }
+}
+  
+
